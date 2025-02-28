9f6f902608fc0e6c055b8d1263c35abcd4719e35
IzumiSy <beetle-noise@gmx.com>
2025-02-28 13:38:19 +0900

    Add code comments

--- src/types.ts
+++ src/types.ts
@@ -1,26 +1,42 @@
-// We are storing our memory using entities, relations, and observations in a graph structure
+/**
+ * The primary nodes in the knowledge graph
+ */
 export type Entity = {
   name: string;
   entityType: string;
   observations: string[];
 };
 
+/**
+ * Relations define directed connections between entities.
+ *
+ * They are always stored in active voice and describe how entities interact or relate to each other
+ */
 export type Relation = {
   from: string;
   to: string;
   relationType: string;
 };
 
-export type KnowledgeGraph = {
-  entities: Entity[];
-  relations: Relation[];
-};
-
+/**
+ * Observations are discrete pieces of information about an entity
+ */
 export type Observation = {
   entityName: string;
   contents: string[];
 };
 
+/**
+ * The knowledge graph is the primary data structure for storing information in the system
+ */
+export type KnowledgeGraph = {
+  entities: Entity[];
+  relations: Relation[];
+};
+
+/**
+ * The KnowledgeGraphManagerInterface is the primary interface for interacting with the knowledge graph
+ */
 export type KnowledgeGraphManagerInterface = {
   createEntities(entities: Entity[]): Promise<Entity[]>;
   createRelations(relations: Relation[]): Promise<Relation[]>;
